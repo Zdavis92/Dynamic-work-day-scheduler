50eaf0f4c18bc388cb3e237c09d04c34f3fb17fd
Zach Davis <zacharydavis92@gmail.com>
2022-01-27 21:40:54 -0600

    created taskbox dynamicly

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
 var currentDay = moment().format("dddd, MMMM Do")
+var currentHour = (moment().format("H"))
+var hoursArr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
+var hoursAmPm = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+// adding the current date to the header
 $("#currentDay").append(currentDay);
+
+var createTimeBlock = function() {
+    for (i = 0; i < hoursArr.length; i++) {
+        var divEl = $("<div>").addClass("row time-block")
+        var hourEl = $("<div>").addClass("hour col-1")
+        var textEl = $("<div>").attr("id", hoursArr[i]).addClass("textarea col-10")
+        var saveEl = $("<div>").addClass("saveBtn col-1").append('<i class="fas fa-save"></i>')
+        hourEl.append(hoursAmPm[i])
+        divEl.append(hourEl, textEl, saveEl)
+        $(".container").append(divEl)
+    }
+}
+
+var checkHour = function() {
+    if (currentHour === parseInt(hoursArr[i])) {
+        textEl.addClass("present")
+    }
+}
+
+createTimeBlock();
