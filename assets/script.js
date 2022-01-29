0d98d4c47df51223eb6ade22ae7054a3643ddbd9
Zach Davis <zacharydavis92@gmail.com>
2022-01-29 13:07:59 -0600

    added task to local storage

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,32 @@ var currentDay = moment().format("dddd, MMMM Do")
 var currentHour = (moment().format("H"))
 var hoursArr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
 var hoursAmPm = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var Tasks = {
+    hour9: "",
+    hour10: "",
+    hour11: "",
+    hour12: "",
+    hour13: "",
+    hour14: "",
+    hour15: "",
+    hour16: "",
+    hour17: "",
+};
 // adding the current date to the header
 $("#currentDay").append(currentDay);
+
+var loadTasks = function() {
+    Tasks = JSON.parse(localStorage.getItem("savedTasks"));
+    $("#09").empty()
+    $("#09").append("<p>" + Tasks.hour9 + "<p>");
+}
 
 var createTimeBlock = function() {
     for (i = 0; i < hoursArr.length; i++) {
         var divEl = $("<div>").addClass("row time-block")
         var hourEl = $("<div>").addClass("hour col-1")
-        var textEl = $("<div>").attr("id", hoursArr[i]).addClass("textarea col-10")
-        var textArea = $("<p>").text("task")
+        var textEl = $("<div>").addClass("textarea col-10").attr("id", hoursArr[i])
+        var textArea = $("<p>")
         var saveEl = $("<div>").addClass("saveBtn col-1").append('<i class="fas fa-save"></i>')
         if (currentHour == parseInt(hoursArr[i])) {
             $(textEl).addClass("present")
@@ -28,7 +45,21 @@ var createTimeBlock = function() {
     }
 }
 
+var saveTasks = function() {
+    Tasks.hour9 = $("#09").text()
+    Tasks.hour10 = $("#10").text()
+    Tasks.hour11 = $("#11").text()
+    Tasks.hour12 = $("#12").text()
+    Tasks.hour13 = $("#13").text()
+    Tasks.hour14 = $("#14").text()
+    Tasks.hour15 = $("#15").text()
+    Tasks.hour16 = $("#16").text()
+    Tasks.hour17 = $("#17").text()
+    localStorage.setItem("savedTasks", JSON.stringify(Tasks))
+}
+
 createTimeBlock();
+loadTasks();
 
 $(".textarea").on("click", "p", function() {
     var text = $(this).text().trim();
@@ -41,4 +72,5 @@ $(".saveBtn").on("click", function() {
     var text = $("textarea").val();
     var taskP = $("<p>").text(text)
     $("textarea").replaceWith(taskP)
+    saveTasks();
 });
