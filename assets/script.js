741a3caf1cb8f6eb6d9d6462b5d8d3a174629909
Zach Davis <zacharydavis92@gmail.com>
2022-01-29 16:15:03 -0600

    added comments to code

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,28 @@ var currentDay = moment().format("dddd, MMMM Do")
 var currentHour = (moment().format("H"))
 var hoursArr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
 var hoursAmPm = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-var Tasks = {
-    hour9: "",
-    hour10: "",
-    hour11: "",
-    hour12: "",
-    hour13: "",
-    hour14: "",
-    hour15: "",
-    hour16: "",
-    hour17: "",
-};
+var Tasks = {};
 // adding the current date to the header
 $("#currentDay").append(currentDay);
 
+// function to load tasks from local storage
 var loadTasks = function() {
     Tasks = JSON.parse(localStorage.getItem("savedTasks"));
+    // if there are no saved tasks then a new defalt list is loaded
+    if (!Tasks) {
+        Tasks = {
+            hour9: "Click to add a new task",
+            hour10: "Click to add a new task",
+            hour11: "Click to add a new task",
+            hour12: "Click to add a new task",
+            hour13: "Click to add a new task",
+            hour14: "Click to add a new task",
+            hour15: "Click to add a new task",
+            hour16: "Click to add a new task",
+            hour17: "Click to add a new task"
+    }};
+
+    // puts to saved tasks into the correct timeslot
     $("#09").empty()
     if (Tasks.hour9 === "") {
         $("#09").append("<p>Click to add a new task<p>")
@@ -83,12 +89,13 @@ var loadTasks = function() {
     }
 }
 
+// function to generate the time blocks on screen
 var createTimeBlock = function() {
     for (i = 0; i < hoursArr.length; i++) {
         var divEl = $("<div>").addClass("row time-block")
         var hourEl = $("<div>").addClass("hour col-1")
         var textEl = $("<div>").addClass("textarea col-10").attr("id", hoursArr[i])
-        var textArea = $("<p>")
+        var textArea = $("<p>").text("Click to add a new task")
         var saveEl = $("<div>").addClass("saveBtn col-1").append('<i class="fas fa-save"></i>')
         if (currentHour == parseInt(hoursArr[i])) {
             $(textEl).addClass("present")
@@ -106,6 +113,7 @@ var createTimeBlock = function() {
     }
 }
 
+// get text and puts into the Tasks object to save them
 var saveTasks = function() {
     Tasks.hour9 = $("#09").text()
     Tasks.hour10 = $("#10").text()
@@ -118,17 +126,18 @@ var saveTasks = function() {
     Tasks.hour17 = $("#17").text()
     localStorage.setItem("savedTasks", JSON.stringify(Tasks))
 }
-
+// starting the app
 createTimeBlock();
 loadTasks();
 
+// creates a text area to add new tasks
 $(".textarea").on("click", "p", function() {
     var text = $(this).text().trim();
     var textInput = $("<textarea>").addClass("textBox").val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
-
+// puts new task on screen and calls the save function
 $(".saveBtn").on("click", function() {
     var text = $("textarea").val();
     var taskP = $("<p>").text(text)
